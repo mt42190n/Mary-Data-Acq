0c46404c1f75435a548e9b1d52ab0388339d5656
Mary Tracy <mary.tracy@spsmail.cuny.edu>
2014-09-26 23:07:18 -0400

    Quiz5

---
+++ Quiz5.R
@@ -0,0 +1,35 @@
+# 1. Data Tidying
+
+# 2. Each variable forms a column and each 
+  #  observation forms a row.
+
+# 3. Column Headers are values instead of variable names
+  #  One observational unit is stored in multiple tables
+  #  More than one observational unit is stored in a single table
+  #  More than one variable is stored in a column
+  #  Variables are stored in rows and column
+
+# 4. Filter=to extract or dwindle down the data to what is needed for the main focus
+  #  Transform=to add or remove variables; to adjust or rearrange the format
+  #  Aggregate=to merge or group variables into one
+  #  Sort=to change the order or sequence of the rows
+
+# 5. subset function
+
+
+# -------------
+
+
+# 1. dplyr is faster and easier to use; it has a lot of convenient and useful functions. 
+  #  However, dplyr can only be used with dataframes.Also, dplyr allows you to chain your 
+  #  operation together.
+
+# 2. With apply, you have to know exactly which function to use. plyr is intuitive, does 
+  #  the complete split-apply-combine, and summarizes the results for you. Essentially, 
+  #  plyr is much more user friendly.
+
+
+# --------------
+
+
+# 1. the gather and spread functions
