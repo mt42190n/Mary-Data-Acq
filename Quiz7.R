a5d700ad487ed37645e6caac50bed0c6d1211064
Mary Tracy <mary.tracy@spsmail.cuny.edu>
2014-10-12 22:08:47 -0400

    Quiz 7

---
+++ Quiz7.R
@@ -0,0 +1,21 @@
+# 1. When you are combined data that has two different formats for date, using 
+  #  regular expression would be less work than using string funtions. One example
+  #  is using "." to represent a character, therefore (....)-(..)-(..) can easily
+  #  get you YYYY MM DD
+
+# 2. R Markdown is a practicle way to keep track of your progress and work while 
+  #  putting it in a presentable format for a presentation or report.The R chunk 
+  #  aspect allows the output of codes you choose to be present in the final 
+  #  document. It's this practicality that makes R Markdown a tool for 
+  #  reproducible research.
+
+# 3. The correlation you determine from a scatterplot assumes that those two 
+  #  variables on the graph are isolated. When conducting a study and gathering
+  #  data you must keep in mind other factors. The example of checking number of 
+  #  hours studied with final exam grade. Perhaps those students who studied 
+  #  more are smarter to begin with and that is really why they got the good grade.
+
+# 4. We use variable coding to turn qualitative data into quantitative data. At
+  #  my old job we conducted a blind peer review for a research grant and we had to 
+  #  code all the names into numbers to make sure no one knew who they were reviewing 
+  #  and no one knew who reviewed them (except the research team).
