7e2d859c1fc1062174c958343daeb6c7a4e700df
Mary Tracy <mary.tracy@spsmail.cuny.edu>
2014-10-19 23:43:07 -0400

    Quiz8

---
+++ Quiz8.R
@@ -0,0 +1,19 @@
+# 1. Beamer is more flexible, and can be more creative with it as it has 
+# different packages. Also working with images and transgering material 
+# is a lot easier.
+
+# 2. You can install a package just for the specific project you want,
+# but it can be easily reproducible. Also projects can be saved in your
+# private package library. This means you don't have to be stuck with a 
+# package if it isn't working for you, and you don't have to apply it to
+# all of your projects.
+
+# 3. 
+```{r ___, include=FALSE}
+
+# 4.
+```{r ___, results='hide'}
+
+# 5. To use python throughout the entire RMarkdown:
+opts_chunk$set(engine = 'python')
+
